94bb82253fdf5779859dd7f1940b41d1bddfc0c3
ShubhamVerma453 <sv4537373@gmail.com>
2023-06-17 11:35:46 +0530

    navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { UseGloblaContext } from "../context"
 
 const NavBar = () => {
+    const { setSearchTerm, fetchRandomMeal } = UseGloblaContext();
+    const [text, changeText] = useState("");
+    function handelChange(event) {
+        changeText(event.target.value);
+        // console.log(text)
+    }
+    function handelSubmit(event) {
+        event.preventDefault();
+        setSearchTerm(text);
+    }
+    function handelRandomMeal() {
+        setSearchTerm("");
+        changeText("");
+        fetchRandomMeal();
+    }
     return (
         <div className="regular-padding">
             <h1><a className="navbar-brand" href="#">Yumm Recipe</a></h1>
             <div className="">
-                <form className="" role="search">
-                    <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                <form className="" onSubmit={handelSubmit} role="search">
+                    <input className="form-control" type="text" value={text} onChange={handelChange} placeholder="Search" />
                     <div className="text-center mt-3">
                         <button className="btn me-5" type="submit">Search</button>
-                        <button className="btn" type="submit">Random</button>
+                        <button className="btn" type="btn" onClick={handelRandomMeal}>Random</button>
                     </div>
                 </form>
             </div>
--- src/context.jsx
+++ src/context.jsx
@@ -1,20 +1,21 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 const AppContext = React.createContext();
 
-const mealByFirstLetter = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Indian";
+const mealURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const randomMeal = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-const AppProvider = ({children}) => {
+const AppProvider = ({ children }) => {
     const [meals, setMeals] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
 
-    const fetchData = async (url) => {
+    const fetchData = async (url) => {    // loads meal on basis of API
         setLoading(true);
         try {
             const data = await axios(url);
-            if(data.data.meals){
+            if (data.data.meals) {
                 setMeals(data.data.meals);
             } else {
                 setMeals([]);
@@ -26,15 +27,24 @@ const AppProvider = ({children}) => {
         setLoading(false);
     }
 
-    useEffect(()=>{
-        // console.log("data fetching");
-        fetchData(mealByFirstLetter);       
-    },[])
+    const fetchRandomMeal = () => {
+        fetchData(randomMeal);
+    }
 
-    return <AppContext.Provider value={{loading, meals}} >
+    useEffect(() => {        // loads some default meals on first render
+        // console.log("data fetching");
+        fetchData(mealURL);
+    }, [])
+
+    useEffect(() => {        // loads meal is there is any search text present
+        if (!searchTerm) return;
+        fetchData(`${mealURL}${searchTerm}`);
+    }, [searchTerm])
+
+    return <AppContext.Provider value={{ loading, meals, setSearchTerm, fetchRandomMeal }} >
         {children}
     </AppContext.Provider>
 }
 
 export const UseGloblaContext = () => useContext(AppContext);
-export {AppContext, AppProvider}
+export { AppContext, AppProvider }
